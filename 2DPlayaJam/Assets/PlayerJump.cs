bdda7be86a7d9eff2b0c20ad190dc996b69a2079
Albert Sanchez <50465984+AlbertSanchezMejia@users.noreply.github.com>
2022-06-11 11:21:06 -0500

    ProtoJump

--- 2DPlayaJam/Assets/PlayerJump.cs
+++ 2DPlayaJam/Assets/PlayerJump.cs
@@ -4,43 +4,7 @@ using UnityEngine;
 
 public class PlayerJump : MonoBehaviour
 {
-    Rigidbody2D rb2D;
-    [SerializeField] int jumpForce;
-    [SerializeField] int gravityScale;
-    [SerializeField] float rayLength;
 
-    [SerializeField] bool onGround;
 
-    void Start()
-    {
-        rb2D = GetComponent<Rigidbody2D>();
-    }
-
-    void Update()
-    {
-        Jump();
-        onGround = Physics2D.Raycast(transform.position - (transform.up * 0.6f), -transform.up, rayLength);
-    }
-
-    void Jump()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
-        {
-            if (onGround)
-            {
-                rb2D.AddForce(transform.up * jumpForce * 100);
-            }
-        }
-    }
-
-    void Gravity()
-    {
-        rb2D.AddForce(-transform.up * gravityScale);
-    }
-
-    void FixedUpdate()
-    {
-        Gravity();
-    }
 
 }
--- 2DPlayaJam/Assets/PlayerMovement.cs
+++ 2DPlayaJam/Assets/PlayerMovement.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [HideInInspector] public Rigidbody2D rb2D;
     [SerializeField] int movementSpeed;
-    Rigidbody2D rb2D;
     Vector3 xMov;
     Vector3 yMov;
+
+    [SerializeField] int jumpForce;
+    [SerializeField] int gravityScale;
+    [SerializeField] float rayLength;
+    bool onGround;
+    public LayerMask groundLayer;
 
     void Start()
     {
@@ -17,12 +23,41 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         rb2D.velocity = (xMov * movementSpeed * Time.fixedDeltaTime * 10) + yMov;
+        Gravity();
+    }
+
+    void Gravity()
+    {
+        rb2D.AddForce(transform.up * gravityScale);
     }
 
     void Update()
     {
         xMov = transform.right * Input.GetAxis("Horizontal");
         yMov = transform.up * rb2D.velocity.y;
+
+        Jump();
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+        {
+            bool onGround1 = Physics2D.Raycast(transform.position + (Vector3.right * 0.35f), Vector2.down, rayLength, groundLayer);
+            bool onGround2 = Physics2D.Raycast(transform.position + (Vector3.right * -0.35f), Vector2.down, rayLength, groundLayer);
+
+            if (onGround1 || onGround2)
+            {
+                rb2D.AddForce(transform.up * jumpForce * 100);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + (Vector3.down * rayLength) + (Vector3.right * 0.35f));
+        Gizmos.DrawLine(transform.position, transform.position + (Vector3.down * rayLength) + (Vector3.right * -0.35f));
     }
 
 }
